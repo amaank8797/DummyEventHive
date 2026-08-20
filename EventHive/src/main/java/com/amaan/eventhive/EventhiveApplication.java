f8d107b16785becb1f91aa26930a4dcb92a710d4
Amaan <96060017+amaank8797@users.noreply.github.com>
2026-08-21 01:31:31 +0530

    Add files via upload

---
+++ EventHive/src/main/java/com/amaan/eventhive/EventhiveApplication.java
@@ -0,0 +1,12 @@
+package com.amaan.eventhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventhiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventhiveApplication.class, args);
+    }
+}
